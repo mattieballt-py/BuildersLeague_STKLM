c18067346e3811dffd4d5c36d2b296292fc332af
mattieballt-py <Mbt.jghs@gmail.com>
2025-01-28 23:46:11 +0000

    First commit

--- Playing.py
+++ Playing.py
@@ -1,4 +1,10 @@
 import numpy as np
 
+"""
+code to update to git:
+git add .  
+git commit -m "Updated feature X"
+git push origin main
+"""
 x = np.arange(0,12,2)
 print("Why isn't this working???")

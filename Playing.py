8897a80d1c3283dc09e04652a1cd6c15a6846729
mattieballt-py <Mbt.jghs@gmail.com>
2025-01-29 15:29:08 +0000

    Added missing Python files

--- Playing.py
+++ Playing.py
@@ -4,7 +4,10 @@ import numpy as np
 code to update to git:
 git add .  
 git commit -m "Updated feature X"
-git push origin main
+git add .
+
 """
 x = np.arange(0,12,2)
 print("Why isn't this working???")
+# testing testing
+# testtttttt
---
+++ testing22.py
@@ -0,0 +1,7 @@
+"""
+why is it updating but not showing on git???
+"""
+import random as rd   
+
+x = rd.randint(0,1)
+print(x)
